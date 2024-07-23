b44527727b8942ba2873b0f1fd70969a4464ac70
ecsdantas <evandson.dantas@msdconsult.com.br>
2024-07-22 21:12:44 -0300

    Fix flip bug

--- src/SimNodeModel.jsx
+++ src/SimNodeModel.jsx
@@ -117,24 +117,26 @@ class DisplayNodeWidget extends React.Component {
         const Icon = node.icon;
         const isSelected = node.isSelected();
         const isFlip = node.flip
+        const InPorts = () => <div className='element-portsIn'>{node.portsIn && node.portsIn.map( inPort =>
+            <PortWidget engine={engine} port={inPort} key={`in-${inPort.getOptions().name}`}>
+                <div className='port-widget' title={inPort.getOptions().name} />
+            </PortWidget>
+        )}</div>
+        const OutPorts = () => <div className='element-portsOut'>{node.portsOut && node.portsOut.map( outPort =>
+            <PortWidget engine={engine} port={outPort} key={`out-${outPort.getOptions().name}`}>
+                <div className='port-widget' title={outPort.getOptions().name} />
+            </PortWidget>
+        )}</div>
+
         const Settings = node.settings;
         return (
             <div className={`element ${isSelected? 'selected' : ''}  ${isFlip? 'flip' : ''}`}>
                 <div className='element-body'>
-                    <div className='element-portsIn'>{node.portsIn && node.portsIn.map( inPort =>
-                        <PortWidget engine={engine} port={inPort} key={`in-${inPort.getOptions().name}`}>
-                            <div className='port-widget' title={inPort.getOptions().name} />
-                        </PortWidget>
-                    )}</div>
-                    
+                    { isFlip? <OutPorts /> : <InPorts /> }
                     <div className='element-icon' title={node.getOptions().name}>
                         {Icon && <Icon />}
                     </div>
-                    <div className='element-portsOut'>{node.portsOut && node.portsOut.map( outPort =>
-                        <PortWidget engine={engine} port={outPort} key={`out-${outPort.getOptions().name}`}>
-                            <div className='port-widget' title={outPort.getOptions().name} />
-                        </PortWidget>
-                    )}</div>
+                    { isFlip? <InPorts /> : <OutPorts /> }
                 </div>
                 <div className="element-label">
                     <span>{node.kind}</span>
