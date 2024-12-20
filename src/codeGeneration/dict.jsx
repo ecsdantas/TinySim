bf9e205406c12993fa7950b3f9be7ff362a27807
Evandson Dantas <evandson.dantas@msdconsult.com.br>
2024-12-20 18:06:33 -0300

    almost done codeGen v0.0.1

--- src/codeGeneration/dict.jsx
+++ src/codeGeneration/dict.jsx
@@ -17,23 +17,31 @@ const ModelActions = {
         return `model->data.${node.CGenUID}`;
     },
     AddModel: (node) => {
+        // Captura as portas de entrada
         const inPorts = node.getInPorts()
+        // Cria uma variável
+        const varname = `var_${node.CGenUID}_${inPorts.length}`
+        // Verifica se já foi criada, em caso afirmativo só retorna a variável
+        if (inUseVariables.find(iuVars => iuVars === varname)){
+            return varname
+        }
         ModelActions.addLib({
             name: "add",
             declaration: `double add(const double* array, int size);`,
-            implementation: `double add(const double* array, int size) {
+            implementation:
+`double add(const double* array, int size) {
     double sum = 0.0;
     for (int i = 0; i < size; i++) { sum += array[i]; }
     return sum;
 }`
         });
-        // Cria uma variável
+        // Variável não existe
         const inputs = inPorts.map((_,i) => ModelActions.getNode(node.getNodeByInput(i)))
         const add_param_var = createNewVar('add_param');
         ModelActions.addStep(`double ${add_param_var}[] = {${inputs.join(',')}}`);
-        const sum_result = createNewVar('sum_result');
-        ModelActions.addStep(`double ${sum_result} = add(${add_param_var}, ${inputs.length})`);
-        return sum_result;
+        ModelActions.addStep(`double ${varname} = add(${add_param_var}, ${inputs.length})`);
+        inUseVariables.push(varname);
+        return varname;
     },
 
     DisplayModel: (node) => {
